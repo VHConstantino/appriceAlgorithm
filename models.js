075e7d4692be05fc6582a462a2c2cb0eb5be9f3e
Eleanor Howard <ellie@lizthedeveloper.com>
2016-12-25 17:39:57 -0800

    started on building up the graph class

---
+++ models.js
@@ -0,0 +1,30 @@
+'use strict'
+
+function Graph(){
+    this.nodes = [];
+    this.edges = [];
+}
+
+Graph.prototype.addNode = function (config) {
+    this.nodes.push(config);
+};
+
+Graph.prototype.addEdge = function (config) {
+    this.edges.push(config);
+};
+
+Graph.prototype.updatePrice = function (productIdNum, storeIdNum, newWeight) {
+    for (var i = 0; i < this.edges.length; i++) {
+        if(this.edges[i].productId === productIdNum && this.edges[i].storeId === storeIdNum) {
+            this.edges[i].weight = newWeight
+        }
+    }
+};
+
+//nodes should look like this:
+// {id: 1234, name: 'wonder bread', type: 'product'} this is a product node
+// {id: 236, name: 'Safeway011', type: 'store', latitude:'36.7783° N', longitude: '119.4179° W'} this is a store nodes
+
+//edges should look like this:
+// {productId: 1234, storeId: 236, weight: 2.99}
+//this edge indicates that wonder bread costs 2.99 at a safeway located  at 36.7783° N & 119.4179° W
